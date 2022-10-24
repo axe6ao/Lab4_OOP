c03410785cd18d2204b437886b4fccc67c6e6702
axe6ao <axe7ao@gmail.com>
2022-10-24 13:46:15 +0300

    First commit

---
+++ First/AttributeManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace First
+{
+    public abstract class AttributeManager
+    {
+        public virtual void Show(params object[] values)
+        {
+            foreach (object type in values)
+            {
+                Debug.Print($"{type.GetType()} - {type}");
+            }
+        }
+    }
+}
---
+++ First/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAttManager concreteAttManager = new ConcreteAttManager();
+            concreteAttManager.Show(15, "name", DateTime.Now);
+        }
+    }
+}
